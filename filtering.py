7414c6258b26035d26b1e7e47a625b8db7be9cb2
Kevin Lacker <lacker@gmail.com>
2021-10-25 14:59:20 -0700

    more detailed chunk analysis

--- filtering.py
+++ filtering.py
@@ -23,9 +23,31 @@ height, _, width = data.shape
 num_chunks = 64
 assert width % num_chunks == 0
 chunk_size = width // num_chunks
-for i in range(num_chunks):
+
+total_col = 0
+
+for i in range(0, num_chunks):
     array = data[:, 0, (i * chunk_size):((i+1) * chunk_size)]
+
+    # Blank out the exact middle, that's the DC spike
+    array[:, chunk_size // 2] = 0
+
     mean = np.mean(array)
     factor = 2
-    over = (array > (factor * mean)).sum()
-    print(f"chunk {i} has mean {mean:.1f} and {over} pixels over {factor}x the mean")
+    threshold = factor * mean
+    high_pixel = (array > threshold).sum()
+    colmax = array.max(axis=0)
+    high_col = (colmax > threshold).sum()
+
+    total_col += high_col
+
+    if not high_pixel:
+        continue
+    
+    print(f"chunk {i}")
+    print(f"  mean: {mean:.1f}")
+    print(f"  {high_pixel} interesting pixels, over {factor}x the mean")
+    print(f"  {high_col} interesting columns")
+
+print()
+print(f"{total_col} interesting columns in total")
