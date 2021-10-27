8de118688da015b93b02ef75e7c85acc5b31a28d
Kevin Lacker <lacker@gmail.com>
2021-10-27 14:16:49 -0700

    try pixel based signal scan method

--- filtering.py
+++ filtering.py
@@ -28,12 +28,14 @@ assert width % num_chunks == 0
 chunk_size = width // num_chunks
 
 total_col = 0
+total_sig = 0
 
 for i in range(0, num_chunks):
     array = xp.array(data[:, 0, (i * chunk_size):((i+1) * chunk_size)])
 
     # Blank out the exact middle, that's the DC spike
-    array[:, chunk_size // 2] = 0
+    midpoint = chunk_size // 2
+    array[:, midpoint] = 0
 
     mean = array.mean()
     factor = 2
@@ -52,5 +54,15 @@ for i in range(0, num_chunks):
     print(f"  {high_pixel} interesting pixels, over {factor}x the mean")
     print(f"  {high_col} interesting columns")
 
+    # Find local signal
+    signal = array.copy()
+    signal -= xp.roll(array, -1, axis=1)
+    signal -= xp.roll(array, 1, axis=1)
+    sig_pixel = (signal > 0).sum()
+    total_sig += sig_pixel
+    print(f"  {sig_pixel} pixels with signal, according to the local scan algorithm")
+    
+    
 print()
 print(f"{total_col} interesting columns in total")
+print(f"{total_sig} sig pixels in total")
