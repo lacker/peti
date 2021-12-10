80badbd0183b9addc1cd77e461b0aecdf1a9f90f
Kevin Lacker <lacker@gmail.com>
2021-12-10 15:49:40 -0800

    least squares regression to find signal drift

--- fitter.py
+++ fitter.py
@@ -13,6 +13,11 @@ class Fitter(object):
         self.data stores the data
         self.mean, self.std store the noise model
         self.mask stores which points are modeled by noise
+
+        self.num_pixels is the number of pixels in the signal
+        self.start_index is the index the signal begins at
+        self.drift_rate is the rate of signal drift (in horizontal pixels per vertical pixel)
+        self.mse is the mean squared error (horizontal distance) from the fit line to the signal pixel
         """
         self.data = data
 
@@ -36,4 +41,12 @@ class Fitter(object):
                 return
 
             raise ValueError(f"we went backwards during sigma clipping, from {in_bounds.size} points to {new_in_bounds.size} points")
+
+        # Do a linear regression
+        row_indexes, col_indexes = xp.where(xp.logical_not(self.mask))
+        self.num_pixels = len(row_indexes)
+        inputs = xp.vstack([row_indexes, xp.ones(len(row_indexes))]).T
+        solution, residual, _, _ = xp.linalg.lstsq(inputs, col_indexes, rcond=None)
+        self.drift_rate, self.start_index = solution
+        self.mse = residual / self.num_pixels
 
