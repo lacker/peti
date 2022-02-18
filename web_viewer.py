d8a7755570d9cb0be9c6862a028e46e4b7390b48
Breakthrough Listen Observer <obs@gb-blh0>
2022-02-18 13:44:48 -0500

    web viewer

---
+++ web_viewer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+A server for a web viewer that lets you look through peti events.
+"""
+
+import cherrypy
+
+class WebViewer(object):
+    @cherrypy.expose()
+    def index(self):
+        return "hello peti web viewer world"
+
+if __name__ == "__main__":
+    cherrypy.config.update({
+        "server.socket_host": "0.0.0.0",
+        "server.socket_port": 8000,
+    })
+
+    cherrypy.quickstart(WebViewer())
